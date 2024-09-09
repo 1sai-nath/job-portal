c673746ff192534b3ad113f0e6e6b8d2441beb41
sai-nath <ssjawadwar@gmail.com>
2024-09-09 17:31:54 +0530

    navbar and login and sign up done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/shared/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Home from "./components/Home";
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  
+]);
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-500'>lets build job portal
-      </h1>
-    </div>
-  )
-}
+    <>
+     <RouterProvider router={appRouter}/> 
+    </>
+  );
+};
 
-export default App
+export default App;
